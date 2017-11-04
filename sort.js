962d190453233b583ea37f6b0b4c36eefbb330e3
rupav jain <rupavwinchester@gmail.com>
2017-11-04 19:41:30 +0530

    Added conditional check in bubble sort.
    To avoid iterating even after sorting.
    Cleaned up code.
    
    Cleaned up code in bubble sort.
    
    Aligned changes properly.
    
    Replace tabs with spaces.
    
    Adjust correct spaces.

--- sort.js
+++ sort.js
@@ -14,11 +14,17 @@ function swap(array, i, j) {
 
 function bubbleSort(a) {
   var n = a.length;
+  var sorted;
   for (var i = 0; i < n; i++) {
+    sorted = true;
     for (var j = 0; j < n - i - 1; j++) {
       if (test(a, j + 1, j) < 0) {
+        sorted = false;
         swap(a, j, j + 1);
       }
+    }
+    if(sorted == true){
+        break;
     }
   }
 }
